661895fff91658e6e20a5524f94587fd4a27eca0
RolexJit <jitsamanta292@gmail.com>
2026-01-28 14:58:29 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,628 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Jit Samanta – Web Developer & Digital Creator</title>
+
+    
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="styles.css">
+
+</head>
+
+
+
+<body>
+    <section>
+<nav class="navbar" style="background:#0a0a2a;">
+  <div class="logo" style="color: white; font-size:30px;">jit.</div>
+
+  <ul class="nav-links" id="navLinks">
+    <li><a href="#">Home</a></li>
+    <li><a href="#about">About</a></li>
+    <li><a href="#">Skills</a></li>
+    <li><a href="#">Projects</a></li>
+    <li><a href="#" class="contact-btn">Contact</a></li>
+  </ul>
+
+  <div class="right">
+    <div class="social-links">
+      <a href="https://linkedin.com" target="_blank">
+        <i class="fab fa-linkedin-in"></i>
+      </a>
+      <a href="https://github.com" target="_blank">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <i class="fas fa-bars menu-toggle" onclick="toggleMenu()"></i>
+  </div>
+</nav>
+    </section>
+   
+ <section class="jit-hero" style="background:#05051a;">
+
+    <div class="container">
+
+        <div class="row align-items-center jit-hero-row">
+
+
+
+            <!-- Left Content -->
+
+            <div class="col-lg-6 col-md-7 jit-hero-content">
+
+                <h5 class="jit-hero-greet" style="color:white;">Hello,</h5>
+
+                <h1 class="jit-hero-title" style="color: white;">I Am <span>Jit.</span></h1>
+
+                <h4 class="jit-hero-role">Web Developer</h4>
+
+                <p class="jit-hero-text" style="color: white;">
+
+                    I am a creative and detail-oriented Web Developer with expertise in building responsive, user-friendly websites using modern web technologies, focused on clean design, performance optimization, and delivering engaging digital experiences.
+
+                </p>
+
+                <a href="#" class="jit-hero-btn">
+
+                    Hire Me <span>→</span>
+
+                </a>
+
+                <a href="Jit_Samanta_CV.pdf" download class="jit-hero-btn">
+
+                    My CV
+
+                </a>
+
+            </div>
+
+
+
+            <!-- Right Image -->
+
+            <div class="col-lg-6 col-md-5 jit-hero-image">
+
+                <img src="jit.jpg" alt="Aliza">
+
+            </div>
+
+
+
+        </div>
+
+    </div>
+
+</section>
+
+
+
+
+
+
+
+<section id="about"class="jit-about-section" style="background:#05051a;">
+
+    <div class="container jit-about-container">
+
+        <!-- Left -->
+
+        <div class="jit-about-left">
+
+            <div class="jit-about-subtitle" >About Me</div>
+
+            <h2 class="jit-about-title" style="color:white;">Building Scalable & User-Centric Web Solutions</h2>
+
+            <p class="jit-about-description" style="color: white;">
+
+                I am a passionate Full Stack Developer with experience in building responsive, scalable web applications using modern frontend and backend technologies, skilled in developing end-to-end solutions, integrating APIs, managing databases, and delivering clean, efficient, and user-focused digital products.
+
+            </p>
+
+
+             
+
+
+            <div class="jit-about-note">
+
+                <div class="jit-note-item" style="color:white;"><i class="fas fa-check-circle"></i> Visionary Creative Designer</div>
+
+                <div class="jit-note-item" style="color: white;"><i class="fas fa-check-circle"></i> Web And Mobile Design</div>
+
+                <div class="jit-note-item" style="color: white;"><i class="fas fa-check-circle"></i> Unlimited Design</div>
+
+            </div>
+
+
+
+            <button class="jit-about-btn">Let's Talk</button>
+
+
+
+            
+
+        </div>
+
+
+
+        <!-- Right -->
+
+        <div class="jit-about-right">
+
+            <img src="jit-3.jpeg" alt="About Me" class="jit-about-image">
+
+        </div>
+
+    </div>
+
+
+
+    
+
+</section>
+
+
+
+
+
+
+
+<section class="jit-skills" style="background: #05051a;">
+
+    <div class="jit-main">
+
+        <div class="jit-sub-main">
+
+
+
+            <div class="jit-top-row">
+
+                
+
+                <h4 style="color: white;">My Skills</h4>
+
+            </div>
+
+
+
+            <div class="jit-midil-row">
+
+
+
+                <div class="jit-top-section">
+
+                    <div class="jit-colum">
+
+                        <div class="jit-icon"><i class="fas fa-database"></i></div>
+
+                        <div class="jit-content">
+
+                            <div class="jit-header-skill">Web Development</div>
+
+                            <div class="jit-paregraph">Html & Css</div>
+
+                        </div>
+
+                    </div>
+
+
+
+                    <div class="jit-colum">
+
+                        <div class="jit-icon1"><i class="fas fa-database"></i></div>
+
+                        <div class="jit-content">
+
+                            <div class="jit-header-skill">Client-Side Development</div>
+
+                            <div class="jit-paregraph">Javascript</div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
+                <div class="jit-top-section">
+
+                    <div class="jit-colum">
+
+                        <div class="jit-icon2"><i class="fas fa-pen-nib"></i></div>
+
+                        <div class="jit-content">
+
+                            <div class="jit-header-skill">Back-end Development</div>
+
+                            <div class="jit-paregraph">PHP & MySQL</div>
+
+                        </div>
+
+                    </div>
+
+
+
+                    <div class="jit-colum">
+
+                        <div class="jit-icon3"><i class="fas fa-microchip"></i></div>
+
+                        <div class="jit-content">
+
+                            <div class="jit-header-skill">OS Development</div>
+
+                            <div class="jit-paregraph">C & C++</div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
+                <div class="jit-top-section">
+
+                    <div class="jit-colum">
+
+                        <div class="jit-icon4"><i class="fas fa-database"></i></div>
+
+                        <div class="jit-content">
+
+                            <div class="jit-header-skill">Website Maintenance & Testing</div>
+
+                            <div class="jit-paregraph">cPanel</div>
+
+                        </div>
+
+                    </div>
+
+
+
+                    <div class="jit-colum">
+
+                        <div class="jit-icon5"><i class="fas fa-pen-nib"></i></div>
+
+                        <div class="jit-content">
+
+                            <div class="jit-header-skill">Frontend Libraries</div>
+
+                            <div class="jit-paregraph">Bootstrap</div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+
+
+
+
+
+
+    <section class="jit_all_designs" id="Portfolio" style="background-color:#05051a;">
+
+        <div class="container">
+
+            <div class="design_heading" style="color:white">My Projects</div>
+
+
+
+            <div class="all_designs_box">
+
+                <div class="design_box">
+
+                    <img class="design_image" src="looplanea.png" alt="design1">
+
+                    <div class="design_text">
+
+                        <h4>Looplane</h4>
+
+                        <p>
+
+                            <a href="https://looplane.in/about.php">
+
+                              a dynamic platform for publishing, sharing, and engaging with articles, ideas and  across various topics.
+
+
+                            </a>
+
+                        </p>
+
+                    </div>
+
+                </div>
+
+
+
+                <div class="design_box">
+
+                    <img class="design_image" src="todo1.png" alt="design2">
+
+                    <div class="design_text">
+
+                        <h4>Todo-list</h4>
+
+                        <p>
+
+                            <a href="https://todo.alexjit.info/" target="_blank">
+
+                               A responsive To-Do List  manage daily tasks with add, update, and delete functionality.
+
+                            </a>
+
+                        </p>
+
+                    </div>
+
+                </div>
+
+
+
+                <div class="design_box">
+
+                    <img class="design_image" src="jit-images.jpg" alt="design3">
+
+                    <div class="design_text">
+
+                        <h4>Ecommerce Website</h4>
+
+                        <p>
+
+                            <a href="https:/ecommerce.alexjit.info/" target="_blank">
+
+                               An ongoing e-commerce web application focused on building  online shopping experience .
+
+                            </a>
+
+                        </p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+
+
+
+
+
+
+    <section class="jit-contact-section" style="background: #05051a;">
+
+        <div class="jit-contact-overlay">
+
+            <div class="jit-contact-container" style="background: #05051a;">
+
+
+
+                <div class="jit-contact-heading">
+
+                    <h2 style="color: white;">Have Any Question?</h2>
+
+                    <p style="color: white;">
+
+                        It is a long established fact that a reader will be distracted<br>
+
+                        content of a page when looking.
+
+                    </p>
+
+                </div>
+
+
+
+                <form class="jit-contact-form">
+
+                    <div class="jit-form-row">
+
+                        <input type="text" placeholder="Your Name*" required>
+
+                        <input type="email" placeholder="Your Email*" required>
+
+                    </div>
+
+
+
+                    <div class="jit-form-row">
+
+                        <input type="text" placeholder="Address">
+
+                        <select>
+
+                            <option selected disabled>Choose Services</option>
+
+                            <option>Web Design</option>
+
+                            <option>Web Development</option>
+
+                            <option>UI / UX Design</option>
+
+                            <option>SEO Optimization</option>
+
+                        </select>
+
+                    </div>
+
+
+
+                    <div class="jit-form-row">
+
+                        <textarea placeholder="Message..."></textarea>
+
+                    </div>
+
+
+
+                    <div class="jit-form-button">
+
+                        <button type="submit">Get In Touch</button>
+
+                    </div>
+
+                </form>
+
+
+
+            </div>
+
+        </div>
+
+    </section>
+
+
+
+    <section class="jit_footer" style="background-color: #05051a;">
+
+        <div class="footer">
+
+
+
+            <div class="footer-logo-name">
+
+                <a href="index.html">
+
+                    Jit<span>.</span>
+
+
+
+                </a>
+
+            </div>
+
+
+
+            <div class="footer_text">
+
+                All rights reserved © 2025
+
+                <a href="index.php" style="color:#38bdf8;">Jit</a>
+
+            </div>
+
+
+
+            <div class="app_links">
+
+                <a href="#">
+
+                    <div class="linking_icon"><i class="fa-brands fa-instagram"></i></div>
+
+                </a>
+
+                <a href="#">
+
+                    <div class="linking_icon"><i class="fa-brands fa-twitter"></i></div>
+
+                </a>
+
+                <a href="#">
+
+                    <div class="linking_icon"><i class="fa-brands fa-dribbble"></i></div>
+
+                </a>
+
+                <a href="#">
+
+                    <div class="linking_icon"><i class="fa-brands fa-behance"></i></div>
+
+                </a>
+
+                <a href="#">
+
+                    <div class="linking_icon"><i class="fa-brands fa-facebook-f"></i></div>
+
+                </a>
+
+                <a href="#">
+
+                    <div class="linking_icon"><i class="fa-solid fa-envelope"></i></div>
+
+                </a>
+
+            </div>
+
+        </div>
+
+    </section>
+
+
+
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+
+    integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+
+    crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+
+    integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+
+    crossorigin="anonymous"></script>
+
+    
+
+    <script>
+<!-- 
+    document.addEventListener("contextmenu", function (e) {
+
+        e.preventDefault();
+
+        alert("Cannot inspect this page!");
+
+    }); -->
+
+
+
+    // Disable F12 key
+
+    document.addEventListener("keydown", function (e) {
+
+        if (e.key === "F12") {
+
+            e.preventDefault();
+
+            alert("Cannot inspect this page!");
+
+        }
+
+    });
+
+    function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("active");
+}
+
+    </script>
+
+
+
+</html>
